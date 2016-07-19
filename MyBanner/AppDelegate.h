ba1bb1a5f37290fd3421cd61835d1391b998a060
snake218 <1294888641@qq.com>
2016-07-19 22:50:06 +0800

    Initial Commit

---
+++ MyBanner/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MyBanner
+//
+//  Created by 小二 on 16/7/19.
+//  Copyright © 2016年 小二. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MyBanner/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MyBanner
+//
+//  Created by 小二 on 16/7/19.
+//  Copyright © 2016年 小二. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
